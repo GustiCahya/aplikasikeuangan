b380cc04cde966626a3beb8397e5575755a90876
Gusti Cahya <gusticahyax@yahoo.com>
2020-01-05 15:41:05 +0800

    ganti about

--- src/components/aboutView.vue
+++ src/components/aboutView.vue
@@ -2,7 +2,7 @@
   <div id="setting">
       <div class="alert alert-success">
           <h3 class="alert-heading">About us</h3>
-          <p>Ini merupakan aplikasi milik OSIS SMKN 2 Mataram dan gunakanlah sebaik mungkin.</p>
+          <p>Ini merupakan aplikasi milik OSIS SMKN 2 Mataram, gunakanlah sebaik mungkin.</p>
           <hr>
           <p>&copy;Copyright OsisSmeda 2020</p>
       </div>
